e4ddb88fd981df698edaed81fca74b80fd7e5f21
Mike Thuchchai J <naki_war@windowslive.com>
2018-02-12 20:28:19 +0700

    intialize unity 3d

---
+++ UI/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+    public void GoToScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void GoToNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
